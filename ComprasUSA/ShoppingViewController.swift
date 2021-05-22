fd796efb240607d2318e768f326cea1560391494
diogenesNegreiros <diogenesdesouza@yahoo.com.br>
2021-05-22 19:58:56 -0300

    UI commit

--- ComprasUSA/ShoppingViewController.swift
+++ ComprasUSA/ShoppingViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ShoppingViewController: UIViewController {
-
+    @IBOutlet weak var tfGastosUsa: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
 
